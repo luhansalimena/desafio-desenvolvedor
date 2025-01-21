b923fb688d87d355fe1d721bc3bb55e1cae472be
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-20 23:24:05 -0300

    Update Asset model to use MongoDB connection and define fillable properties

--- app/Models/Asset.php
+++ app/Models/Asset.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use MongoDB\Laravel\Eloquent\Model;
 
 class Asset extends Model
 {
+    protected $connection = 'mongodb';
     protected $fillable = [
         'RptDt',
         'TckrSymb',
