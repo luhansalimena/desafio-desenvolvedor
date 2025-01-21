7b9e1b5c0671179b90c61da07ffbef942dac4784
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-20 23:58:14 -0300

    Add OpenAPI documentation for Financial Data API in Controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,5 +4,15 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
-    //
+    /**
+     * @OA\Info(
+     *     version="1.0.0",
+     *     title="Financial Data API",
+     *     description="API for handling file uploads"
+     * )
+     **/
+    public function __construct()
+    {
+        //
+    }
 }
