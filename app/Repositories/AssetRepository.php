1a0705a49739d19922ae0b7f60126b207d564995
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-21 23:15:48 -0300

    Add search functionality to AssetRepository with pagination support

--- app/Repositories/AssetRepository.php
+++ app/Repositories/AssetRepository.php
@@ -4,11 +4,27 @@ namespace App\Repositories;
 
 use App\Models\Asset;
 use App\Repositories\Interface\AssetRepositoryInterface;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class AssetRepository implements AssetRepositoryInterface
 {
     public function insert(array $data): bool
     {
         return Asset::insert($data);
+    }
+
+    public function search(array $searchParams): LengthAwarePaginator
+    {
+        $query = Asset::query();
+
+        if(data_get($searchParams, 'TckrSymb')) {
+            $query->where('TckrSymb', data_get($searchParams, 'TckrSymb'));
+        }
+
+        if(data_get($searchParams, 'RptDt')) {
+            $query->where('RptDt', '=',data_get($searchParams, 'RptDt'));
+        }
+
+        return $query->paginate(100);
     }
 }
--- app/Repositories/Interface/AssetRepositoryInterface.php
+++ app/Repositories/Interface/AssetRepositoryInterface.php
@@ -2,8 +2,11 @@
 
 namespace App\Repositories\Interface;
 
+use Illuminate\Pagination\LengthAwarePaginator;
+
 
 interface AssetRepositoryInterface
 {
     public function insert(array $data): bool;
+    public function search(array $searchParams): LengthAwarePaginator;
 }
