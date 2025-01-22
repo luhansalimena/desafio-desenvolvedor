97d189027df1ae13eb140983e15ee5673f35c199
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-21 22:22:00 -0300

    Add original file name to uploaded file storage

--- app/Actions/File/HandleUploadedFile.php
+++ app/Actions/File/HandleUploadedFile.php
@@ -38,6 +38,7 @@ class HandleUploadedFile
 
         $this->fileRepository->store([
             'fileHash' => $fileHash,
+            'name' => $file->getClientOriginalName(),
         ]);
 
         return $file;
