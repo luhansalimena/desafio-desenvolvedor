918b8eec76d730379292a3be8aefc33dbb6ee43d
Luhan Salimena <luhan@beepayapp.com.br>
2025-01-21 22:22:10 -0300

    Add route for retrieving list of files

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('upload', [FileController::class, 'upload']);
+
+Route::get('files', [FileController::class, 'index']);
